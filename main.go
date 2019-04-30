e077a6328d2513e729585f88bd642c1986c8dd92
Steven Maharaj <maharaj@6200D-143458-M.local>
2019-04-30 15:06:24 +1000

    makeX and makeY

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 func main() {
-	
+
+	X := makeX()
+	Y := makeY(X)
+
+	fmt.Println(Y)
+	fmt.Println(len(Y))
+}
+
+func makeX() []float64 {
+	X := []float64{}
+
+	n := 100
+	for i := 0; i < n; i++ {
+		X = append(X, rand.Float64())
+	}
+	return X
+}
+
+func makeY(X []float64) []float64 {
+	Y := []float64{}
+
+	for _, x := range X {
+		std := 0.01
+		Y = append(Y, x+rand.NormFloat64()*std)
+
+	}
+	return Y
+
 }
